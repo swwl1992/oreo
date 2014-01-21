77fd48f137899bbd301f56edab1f51385954d577
swwl1992 <swwl1992@gmail.com>
2014-01-21 15:55:51 +0800

    [reactive subtitle edition] done

--- client.ml
+++ client.ml
@@ -2,7 +2,7 @@ open Lwt
 open Dom_html
 open Eliom_content.Html5
 open React
-open Subtitle
+open Subtitle.Sub
 
 let simple_example source_input reactive_input =
     let src_input_elt = To_dom.of_input source_input in
@@ -11,7 +11,7 @@ let simple_example source_input reactive_input =
         dest##value <- src##value
     in
     let src, send_src = E.create () in
-    let sync_react = E.map (sync_inputs rea_input_elt) src in
+    let _ = E.map (sync_inputs rea_input_elt) src in
     let run () = send_src src_input_elt in
     let _ = window##setInterval(Js.wrap_callback run, 50.)
     in ()
@@ -20,14 +20,10 @@ let media_init vid =
     let vid_elt = Interface.To_dom.of_video vid in
     let reset_btn_elt = createButton document in
     let src_input_elt = createInput document in
+    let sub_text, send_sub_text = E.create () in
+    let _ = E.map (edit_sub_text vid_elt) sub_text in
 
     (* initialization *)
-    let appendWithWrapper bdy elt =
-        let div = createDiv document in
-            Dom.appendChild div elt;
-            Dom.appendChild bdy div
-    in
-
     reset_btn_elt##innerHTML <- Js.string "Reset Source";
     appendWithWrapper document##body reset_btn_elt;
     appendWithWrapper document##body src_input_elt;
@@ -36,26 +32,48 @@ let media_init vid =
     let div = createDiv document in
     Dom.appendChild div vid_elt;
     Dom.appendChild document##body div;
+    let st_input_elt, et_input_elt, txt_textarea_elt, add_btn_elt =
+        appendEditor div in
 
     (* modifying methods *)
     let reset_vid_src vid src =
-        vid##src <- src
+        vid##src <- src;
+        remove_sub div
     in
 
-    let sub_lst = ref
-        [{start_t = 1.; end_t = 3.; text= "hello"; x = 0; y = 0;}] in
+    let add_subtitle vid_elt div =
+        let start_t = float_of_string
+            (Js.to_string st_input_elt##value) in
+        let end_t = float_of_string
+            (Js.to_string et_input_elt##value) in
+        let text = Js.to_string txt_textarea_elt##value in
+        match !sub_lst with
+        | [] ->
+            add_sub start_t end_t text;
+            start_sub vid_elt div
+        | h::t ->
+            add_sub start_t end_t text;
+    in
 
-    let id, sub_div = start_sub vid_elt div !sub_lst in
+    let check_sub txt_textarea_elt () =
+        let text = Js.to_string txt_textarea_elt##value in
+        send_sub_text text
+    in
 
-    sub_lst :=
-        [{start_t = 1.; end_t = 3.; text= "seven"; x = 0; y = 0;}];
-
-    let iid = reset_sub id vid_elt sub_div !sub_lst in
+    let _ = Dom_html.window##setInterval(Js.wrap_callback
+        (check_sub txt_textarea_elt), 50.)
+    in
 
     (* handle button clicks *)
     Lwt.async
     (fun () ->
         let open Lwt_js_events in
+        Lwt.pick[
         clicks reset_btn_elt
-        (fun _ _ -> reset_vid_src vid_elt src_input_elt##value;
-            Lwt.return ()))
+            (fun _ _ -> reset_vid_src vid_elt src_input_elt##value;
+            Lwt.return ());
+        clicks add_btn_elt
+            (fun _ _ -> add_subtitle vid_elt div;
+            Lwt.return ());
+        ]
+    )
--- subtitle.ml
+++ subtitle.ml
@@ -2,103 +2,131 @@ open Js
 open Dom_html
 open Interface
 
-type t = {
-    start_t : float;
-    end_t : float;
-    text : string;
-    x: int;
-    y: int;
-}
+module Sub = struct
+    type t = {
+        start_t : float;
+        end_t : float;
+        text : string;
+        x: int;
+        y: int;
+    }
 
-(* creaet a div element to hold the text *)
-(* the div will float right above the video *)
-let createSubDiv vid_elt =
-    let sub_div = createDiv document in
-    sub_div##style##position <- Js.string "relative";
-    sub_div##style##fontWeight <- Js.string "bold";
-    sub_div##style##color <- Js.string "white";
-    sub_div##style##textAlign <- Js.string "center";
-    sub_div##style##width <-
-        Js.string ((string_of_int vid_elt##videoWidth)^"px");
-    sub_div##style##top <-
-        Js.string ((string_of_int (vid_elt##videoHeight - 65))^"px");
-    sub_div##style##height <- Js.string "15px";
-    sub_div
+    let sub_lst = ref ([] : t list)
+    let id = ref (window##setInterval(Js.wrap_callback
+        (fun () -> ()), 999.))
+    let sub_div = ref (createDiv document)
 
-(* subtitle display function *)
-let display_sub vid_elt div sub =
-    let start_t = sub.start_t in
-    let end_t = sub.end_t in
-    let text = sub.text in
-    let curr_t = Js.to_float vid_elt##currentTime in
-    Firebug.console##log(vid_elt##currentTime);
-    if start_t <= curr_t && curr_t <= end_t then
-        begin
-            div##innerHTML <- Js.string text;
-            true
-        end
-    else
-        false
+    (* helper functions *)
+    let appendWithWrapper bdy elt =
+        let div = createDiv document in
+            Dom.appendChild div elt;
+            Dom.appendChild bdy div
 
-(* subtitle edition function *)
+    (* creaet a div element to hold the text *)
+    (* the div will float right above the video *)
+    let createSubDiv vid_elt =
+        let sub_div = createDiv document in
+        sub_div##style##position <- Js.string "relative";
+        sub_div##style##fontWeight <- Js.string "bold";
+        sub_div##style##color <- Js.string "white";
+        sub_div##style##textAlign <- Js.string "center";
+        sub_div##style##width <- Js.string
+            ((string_of_int vid_elt##videoWidth)^"px");
+        sub_div##style##top <- Js.string
+            ((string_of_int (vid_elt##videoHeight - 50))^"px");
+        sub_div##style##height <- Js.string "0px";
+        sub_div
 
-(* add a new subtitle and append to the list *)
-(* input: start_t end_t text *)
-(* output: new sub_lst *)
-let add_sub st et txt sub_lst =
-    let new_sub = {
-        start_t = st;
-        end_t = et;
-        text = txt;
-        x = 0;
-        y = 0;
-    } in
-    List.append sub_lst [new_sub]
+    (* create a subtitle editor and append it to the wrapper *)
+    let appendEditor div =
+        let st_input = createInput document in
+        let et_input = createInput document in
+        let txt_textarea = createTextarea document in
+        let add_button = createButton document in
+        add_button##innerHTML <- Js.string "Add subtitle";
+        appendWithWrapper div st_input;
+        appendWithWrapper div et_input;
+        appendWithWrapper div txt_textarea;
+        appendWithWrapper div add_button;
+        st_input, et_input, txt_textarea, add_button
 
-(* edit the text *)
-(* input: vid_elt new_text *)
-(* output: new sub_lst *)
-let rec edit_sub_text vid_elt new_text = function
-    | [] -> []
-    | h::t ->
-        let curr_t = vid_elt##currentTime in
-        let _start_t = h.start_t in
-        let _end_t = h.end_t in
-        let _x = h.x in
-        let _y = h.y in
-        if _start_t <= curr_t && curr_t <= _end_t then
-            let new_sub = {
-                start_t = _start_t;
-                end_t = _end_t;
-                text = new_text;
-                x = _x;
-                y = _y
-            } in
-            new_sub::(edit_sub_text vid_elt new_text t)
+    (* subtitle display function *)
+    let display_sub vid_elt div sub =
+        let start_t = sub.start_t in
+        let end_t = sub.end_t in
+        let text = sub.text in
+        let curr_t = Js.to_float vid_elt##currentTime in
+        if start_t <= curr_t && curr_t <= end_t then
+            begin
+                div##innerHTML <- Js.string text;
+                true
+            end
         else
-            h::(edit_sub_text vid_elt new_text t)
+            false
 
-(* cycle through all the subtitles and show the correct one *)
-let rec cycle_sub vid_elt div sub_lst () =
-    match sub_lst with
-    | [] -> div##innerHTML <- Js.string ""
-    | h::t ->
-        if display_sub vid_elt div h then
-            ()
-        else
-            cycle_sub vid_elt div t ()
+    (* subtitle edition functions *)
 
-(* make the subtitle appear on the video element *)
-let start_sub vid_elt div sub_lst =
-    let sub_div = createSubDiv vid_elt in
-    Dom.appendChild div sub_div;
-    let id = Dom_html.window##setInterval(Js.wrap_callback
-            (cycle_sub vid_elt sub_div sub_lst), 50.)
-    in
-    id, sub_div
+    (* add a new subtitle and append to the list *)
+    (* input: start_t end_t text *)
+    let add_sub st et txt =
+        let new_sub = {
+            start_t = st;
+            end_t = et;
+            text = txt;
+            x = 0;
+            y = 0;
+        } in
+        sub_lst := (List.append !sub_lst [new_sub])
 
-(* reset the subtitles after edition *)
-let reset_sub id vid_elt sub_div sub_lst =
-    Dom_html.window##clearInterval(id);
-    Dom_html.window##setInterval(Js.wrap_callback
-        (cycle_sub vid_elt sub_div sub_lst), 50.)
+    (* edit the text *)
+    (* input: vid_elt new_text *)
+    let edit_sub_text vid_elt new_text =
+        let rec find_text vid_elt sub_lst new_text =
+            match sub_lst with
+            | [] -> []
+            | h::t ->
+                let curr_t = Js.to_float vid_elt##currentTime in
+                let _start_t = h.start_t in
+                let _end_t = h.end_t in
+                let _x = h.x in
+                let _y = h.y in
+                if _start_t <= curr_t && curr_t <= _end_t then
+                    let new_sub = {
+                        start_t = _start_t;
+                        end_t = _end_t;
+                        text = new_text;
+                        x = _x;
+                        y = _y
+                    } in
+                    new_sub::(find_text vid_elt t new_text)
+                else
+                    h::(find_text vid_elt t new_text)
+        in
+        sub_lst := find_text vid_elt !sub_lst new_text
+
+
+    (* cycle through all the subtitles and show the correct one *)
+    let start_cycle_sub vid_elt div () =
+        let rec cycle_sub vid_elt div sub_lst =
+            match sub_lst with
+            | [] -> div##innerHTML <- Js.string ""
+            | h::t ->
+                if display_sub vid_elt div h then
+                    ()
+                else
+                    cycle_sub vid_elt div t
+        in
+        cycle_sub vid_elt div !sub_lst
+
+    (* make the subtitle appear on the video element *)
+    let start_sub vid_elt div =
+        sub_div := createSubDiv vid_elt;
+        Dom.insertBefore div !sub_div (Js.some vid_elt);
+        id := Dom_html.window##setInterval(Js.wrap_callback
+                (start_cycle_sub vid_elt !sub_div), 50.)
+
+    let remove_sub div =
+        Dom_html.window##clearInterval(!id);
+        Dom.removeChild div !sub_div;
+        sub_lst := []
+end
